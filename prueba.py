64af880d0c51375c93fab1038ecc9ec8e61ddf9e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-10-16 19:29:02 -0500

    modificacion archivo prueba en nueva rama

--- prueba.py
+++ prueba.py
@@ -1,4 +1,7 @@
 print("Hola Grupo vamos a ver como cambiar el nombre de un archivo y como eliminarlo")
 num1 = 3
 num2 = 5
-resultado = num1, num2 = num1, 
+resultado = num1 + num2
+print(resultado)
+
+print("Nueva línea de código para ver cómo cambiar el nombre de un archivo y cómo eliminarlo")
